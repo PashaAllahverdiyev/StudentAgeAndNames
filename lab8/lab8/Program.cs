b1c02ad776d02a279ee666546a60a2f18e8e5610
PashaAllahverdiyev <pashama@code.edu.az>
2024-03-14 12:11:55 +0400

    student

---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,45 @@
+﻿//int[] arrs = { 19, 20, 23, 24, 25 };
+//Console.WriteLine("Yashinizi yazin: ");
+//input:
+//if(int.TryParse(Console.ReadLine(),out int TelebeYash))
+//{
+//    bool checkYash = false;
+//    foreach(int i in arrs)
+//    {
+//        if(i == TelebeYash)
+//        {
+//            checkYash = true;
+//            break;
+//        }
+//    }
+//    Console.WriteLine(checkYash);
+//}
+//else
+//{
+//    Console.WriteLine("Yashinizi reqemle yazin:");
+//    goto input;
+//}
+
+//Task2
+string[] names = { "Pasha", "Nicat", "Ali","Miragha" };
+l1:
+Console.WriteLine("Ad daxil edin");
+string input = Console.ReadLine();
+int count = 0;
+foreach (var item in names)
+{
+    if (input == item)
+    {
+        count++;
+    }
+}
+    if (count >= 1)
+    {
+        Console.WriteLine("Telebenin adi:"+input);
+    }
+    else
+    {
+        Console.WriteLine("Telebe yoxdur");
+        goto l1;
+    }
+
